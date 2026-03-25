2c35dde83b44fc0ffa8e33ccac68dc95bbcc7adf
Nemanja Mihajlović <nemanjazmaj.mihajlovic@gmail.com>
2026-03-25 09:32:26 +0100

    fix env + add root route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,13 @@
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// 👇 ovo rešava 100% path problem
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// 👇 apsolutna putanja do .env
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config();
 
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("NutriFlow API radi 🚀");
+});
 
 app.listen(PORT, () => {
   console.log(`Server radi na portu ${PORT}`);
