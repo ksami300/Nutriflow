be0f4307f139ea6b87fadaa8ec99dd6453a08e69
Nemanja Mihajlović <nemanjazmaj.mihajlovic@gmail.com>
2026-04-18 08:52:06 +0200

    fix: server final

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,17 @@
-const PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// middleware
+app.use(express.json());
+
+// TEST ROUTE (BITNO ZA RAILWAY)
 app.get("/", (req, res) => {
   res.send("NutriFlow backend radi 🚀");
-}); 
+});
+
+// PORT (RAILWAY koristi process.env.PORT)
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
