992ab1a7726e7b2a25d26203f75bfa1f2c71375a
Nemanja Mihajlović <nemanjazmaj.mihajlovic@gmail.com>
2026-04-18 08:45:13 +0200

    fix: root route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
-const app = require("./app");
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("NutriFlow backend radi 🚀");
+}); 
